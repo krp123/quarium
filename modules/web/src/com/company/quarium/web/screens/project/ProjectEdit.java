e95e575d5abd6e5755c337c1483eea03facb8ef7
Andrey Karpenko <karpenkoandrey92@gmail.com>
2021-03-16 21:02:30 +0400

    Поправил ошибку

--- modules/web/src/com/company/quarium/web/screens/project/ProjectEdit.java
+++ modules/web/src/com/company/quarium/web/screens/project/ProjectEdit.java
@@ -3,10 +3,10 @@ package com.company.quarium.web.screens.project;
 import com.company.quarium.entity.Project;
 import com.company.quarium.entity.Qa;
 import com.company.quarium.entity.QaProjectRelationship;
-import com.haulmont.cuba.core.global.Metadata;
 import com.haulmont.cuba.gui.ScreenBuilders;
 import com.haulmont.cuba.gui.components.Action;
 import com.haulmont.cuba.gui.model.CollectionContainer;
+import com.haulmont.cuba.gui.model.DataContext;
 import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
 
@@ -22,7 +22,7 @@ public class ProjectEdit extends StandardEditor<Project> {
     protected ScreenBuilders screenBuilders;
 
     @Inject
-    protected Metadata metadata;
+    protected DataContext dataContext;
 
     @Inject
     protected CollectionContainer<QaProjectRelationship> qaProjectDc;
@@ -45,7 +45,7 @@ public class ProjectEdit extends StandardEditor<Project> {
     }
 
     private QaProjectRelationship createRelationshipFromQa(Qa qa) {
-        QaProjectRelationship qaProjectRelationship = metadata.create(QaProjectRelationship.class);
+        QaProjectRelationship qaProjectRelationship = dataContext.create(QaProjectRelationship.class);
         qaProjectRelationship.setProject(projectDc.getItem());
         qaProjectRelationship.setQa(qa);
 
