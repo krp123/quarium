0d021a47dccf8ed85d2ce2e50cc56117fad7d8df
krp <karpenkoandrey92@gmail.com>
2021-07-14 17:32:14 +0400

    Поправил роли для новых экранов. Теперь можно заходить в систему при наличии лишь одной из новых ролей.

--- modules/core/src/com/company/quarium/roles/ViewRole.java
+++ modules/core/src/com/company/quarium/roles/ViewRole.java
@@ -1,14 +1,12 @@
 package com.company.quarium.roles;
 
 import com.haulmont.cuba.security.app.role.AnnotatedRoleDefinition;
-import com.haulmont.cuba.security.app.role.annotation.EntityAccess;
-import com.haulmont.cuba.security.app.role.annotation.EntityAttributeAccess;
-import com.haulmont.cuba.security.app.role.annotation.Role;
-import com.haulmont.cuba.security.app.role.annotation.ScreenAccess;
+import com.haulmont.cuba.security.app.role.annotation.*;
 import com.haulmont.cuba.security.entity.EntityOp;
 import com.haulmont.cuba.security.role.EntityAttributePermissionsContainer;
 import com.haulmont.cuba.security.role.EntityPermissionsContainer;
 import com.haulmont.cuba.security.role.ScreenPermissionsContainer;
+import com.haulmont.cuba.security.role.SpecificPermissionsContainer;
 
 @Role(name = ViewRole.NAME)
 public class ViewRole extends AnnotatedRoleDefinition {
@@ -41,12 +39,51 @@ public class ViewRole extends AnnotatedRoleDefinition {
             "ext_quarium_Checklist.edit",
             "quarium_RegressChecklist.edit",
             "quarium_TestCase.edit",
+            "quarium_TestRun.edit",
+            "quarium_TestRunTestSuitBrowse.browse",
+            "quarium_TestSuit.edit",
+            "quarium_Milestone.edit",
             "diffFrame",
             "help",
             "aboutWindow",
-            "settings"})
+            "settings",
+            "thirdpartyLicenseWindow",
+            "runtimePropertiesFrame",
+            "layoutAnalyzer",
+            "fileUploadDialog",
+            "multiuploadDialog",
+            "inputDialog",
+            "backgroundWorkProgressWindow",
+            "backgroundWorkWindow",
+            "addCondition",
+            "date-interval-editor",
+            "customConditionEditor",
+            "customConditionFrame",
+            "dynamicAttributesConditionEditor",
+            "dynamicAttributesConditionFrame",
+            "filterEditor",
+            "ftsConditionFrame",
+            "groupConditionFrame",
+            "propertyConditionFrame",
+            "filterSelect",
+            "saveFilter",
+            "list-editor-popup",
+            "editWindowActions",
+            "extendedEditWindowActions",
+            "saveSetInFolder",
+            "login",
+            "loginWindow",
+            "main",
+            "mainWindow",
+            "notFoundScreen"})
     @Override
     public ScreenPermissionsContainer screenPermissions() {
         return super.screenPermissions();
+    }
+
+    @SpecificAccess(permissions = {"cuba.gui.loginToClient"})
+    @Override
+    public SpecificPermissionsContainer specificPermissions() {
+        return super.specificPermissions();
     }
 }
