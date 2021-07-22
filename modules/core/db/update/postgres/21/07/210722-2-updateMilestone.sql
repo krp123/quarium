404e1ae8e77b2fa54fc0bbe4a101ffd6334fe769
krp <karpenkoandrey92@gmail.com>
2021-07-22 17:47:09 +0400

    Убрал время из дат в milestone

---
+++ modules/core/db/update/postgres/21/07/210722-2-updateMilestone.sql
@@ -0,0 +1,4 @@
+alter table QUARIUM_MILESTONE rename column finish_date to finish_date__u06828 ;
+alter table QUARIUM_MILESTONE rename column start_date to start_date__u53140 ;
+alter table QUARIUM_MILESTONE add column START_DATE date ;
+alter table QUARIUM_MILESTONE add column FINISH_DATE date ;
---
+++ modules/core/db/update/postgres/21/07/210722-2-updateMilestone_DropScript.sql
@@ -0,0 +1,2 @@
+alter table QUARIUM_MILESTONE drop column START_DATE__U53140 cascade ;
+alter table QUARIUM_MILESTONE drop column FINISH_DATE__U06828 cascade ;
