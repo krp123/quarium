d7d23dd8f405f0edf7591c862508708883ab698f
krp <karpenkoandrey92@gmail.com>
2021-08-10 09:39:50 +0400

    Revert "TestSuit для TestRun теперь добавляется по проекту"
    This reverts commit 50e39fbc

--- modules/web/src/com/company/quarium/web/screens/testsuit/TestSuitCaseBrowse.java
+++ modules/web/src/com/company/quarium/web/screens/testsuit/TestSuitCaseBrowse.java
@@ -16,7 +16,6 @@ import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 @UiController("quarium_TestSuitCase.browse")
 @UiDescriptor("test-suit-case-browse.xml")
@@ -47,22 +46,12 @@ public class TestSuitCaseBrowse extends StandardLookup<TestSuit> {
     @Subscribe
     public void onAfterShow(AfterShowEvent event) {
 
-        Map<String, Object> params = this.getWindow().getContext().getParams();
-        if (hasProjectParameter(params)) {
-            findTestSuitByProject(params);
-        }
-
-
         Table testCasesTable = (Table) casesFragment.getFragment().getComponent("testCasesTable");
         tabsheet.addSelectedTabChangeListener(selectedTabChangeEvent -> {
             if (!testCasesTable.getLookupSelectedItems().isEmpty()) {
                 testCasesTable.setSelected(new ArrayList<>());
                 testSuitsTable.setSelected(new ArrayList<>());
-                if (hasProjectParameter(params)) {
-                    findTestSuitByProject(params);
-                } else {
-                    testSuitsDl.load();
-                }
+                testSuitsDl.load();
             }
         });
 
@@ -126,19 +115,5 @@ public class TestSuitCaseBrowse extends StandardLookup<TestSuit> {
             }
         }
         return null;
-    }
-
-    private boolean hasProjectParameter(Map<String, Object> params) {
-        if (params.containsKey("project")) {
-            return true;
-        }
-        return false;
-    }
-
-    private void findTestSuitByProject(Map<String, Object> params) {
-        List<TestSuit> testSuits = dataManager.load(TestSuit.class)
-                .query("select e from quarium_TestSuit e where e.project =:project")
-                .parameter("project", params.get("project")).list();
-        testSuitsDc.setItems(testSuits);
     }
 }
